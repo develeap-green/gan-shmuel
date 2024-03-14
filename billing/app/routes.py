5b1599b9a6966167dc2bb19ee68cbd0be923fabf
alone <alon90620@gmail.com>
2024-03-14 17:22:25 +0200

    Health check v1

--- billing/app/routes.py
+++ billing/app/routes.py
@@ -2,6 +2,7 @@ from app import app
 
 from flask import Flask, abort, request, jsonify
 from flask_mysqldb import MySQL
+import requests
 
 app = Flask(__name__)
 
@@ -27,11 +28,33 @@ def updateProvider(providerId):
 
 
 
+# Until we get db name / ip that for testing
+#resource = "http://127.0.0.1:5000/health"
+#resource = "https://www.goofhjjstgle.com/"
+resource = "https://www.google.com/"
 
-if __name__ == '__main__':
+# Check if DB healthy
+def checkResource(resource):
+    try:
+        # Check the resource status and parse it status code (int)
+        response = requests.get(resource).status_code
+        if response == 200:
+            return "OK"
+        else:
+            return "Failure"
+    # Error raise when domain not found
+    except requests.RequestException:
+        return "Failure"
+
+# Get request check if db is up
+@app.route("/health",methods=["GET"])
+def health_check():
+    resourceStatus = checkResource(resource) # Call checkResource function and check status
+    if request.method == "GET" and resourceStatus == "OK": #if resource found
+        return {"status": "OK", "statusCode": "200 OK", }
+    else: #if resource not found or not good
+        return {"status": "Failure", "statusCode": "500 Internal Server Error"}
+
+    
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-# @app.route('/')
-# def example():
-#     return 200
