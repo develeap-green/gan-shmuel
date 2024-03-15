2d751a81c0c2c4a8f3660deaecbb13c08fdf092c
Daniel Rashba <danirdd92@gmail.com>
2024-03-15 15:45:53 +0200

    feat: add health_check

--- weight/app/routes.py
+++ weight/app/routes.py
@@ -1,5 +1,18 @@
-from app import app
+from app import app, db
+from sqlalchemy.sql import text
+import logging
+from http import HTTPStatus
 
-# @app.route('/')
-# def example():
-#     return 200
+@app.route('/health')
+def health_check():
+    try:
+        db.session.execute(text('SELECT 1'))
+        db.session.close()  
+        # return jsonify({"status": "OK", "code": HTTPStatus.OK}), HTTPStatus.OK
+        return '', HTTPStatus.OK
+    
+    except Exception as err:
+        logging.error(f"Database connection error: {err}")
+        error = {"status": "Service Unavailable", "code": HTTPStatus.SERVICE_UNAVAILABLE}
+        # return jsonify(error), HTTPStatus.SERVICE_UNAVAILABLE
+        return '', HTTPStatus.SERVICE_UNAVAILABLE
