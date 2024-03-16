d4dfa94211f394ad0f709f4a6413c3d4d578081e
איתמר ברטי <itamar.berti8@gmail.com>
2024-03-16 11:20:13 +0200

    post route start

--- weight/app/routes.py
+++ weight/app/routes.py
@@ -19,44 +19,40 @@ def generate_new_session_id():
 def example():
     return 200
 
+
 @app.route('/weight', methods=['POST'])
 def handle_post():
-    try:
+
+        data = request.get_json()
+
         if request.json['direction'] in ["none", "in"]:
             generate_new_session_id()
         
         transaction_obj = Transactions(
-            direction=request.json['direction'],
-            truck=request.json['truck'],
-            containers=request.json['containers'],
-            produce=request.json['produce'],
+            direction = data['direction'],
+            truck = data['truck'],
+            containers = data['containers'],
+            produce = data['produce'],
+            datetime = date_time.now(),
             session_id = auto_increment_number,
-            datetime=date_time.strftime(format)     
         )
-        
-        container_obj = ContainersRegistered(
-            weight=int(request.json['weight']),
-            unit=request.json['unit']
-        )
-        db.session.add(container_obj)
 
-        if request.json['direction']  == 'out':
-            transaction_obj.truckTara
-            transaction_obj.neto
-
-        db.session.add(transaction_obj)
-        db.session.add(container_obj)
-        db.session.commit()
-
+        # if request.json['direction']  == 'out':
+            # transaction_obj.truckTara
+            # transaction_obj.neto
+        try:
+            db.session.add(transaction_obj)
+            db.session.commit()
+        except Exception as err:
+             logging.error(f"Error happened! {err}")
         response_data = {
-            "id": transaction_obj.id,
-            "truck": transaction_obj.truck,
-            "bruto": transaction_obj.bruto
+            "id": "TEST",
+            "truck": "T-1234",
+            "bruto": 5000
         }
     
         return response_data
-    except Exception:
-        return "error happend"
+
 
 @app.route('/health')
 def health_check():
